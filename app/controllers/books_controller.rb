fdee12e88009127832483bfc1576ef42b2c86ea0
vagrant <vagrant@localhost.localdomain>
2020-04-13 06:36:28 +0000

    [Add] Book機能追加

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,41 @@
+class BooksController < ApplicationController
+	def index
+		@books = Book.all
+		@user = current_user
+		@book = Book.new
+	end
+
+	def create
+		@book = Book.new(book_params)
+		@book.user_id = current_user.id
+		@book.save
+		redirect_to book_path(@book)
+	end
+
+	def show
+		@book = Book.find(params[:id])
+		@user = @book.user
+	end
+
+	def edit
+		@book = Book.find(params[:id])
+	end
+
+	def update
+		@book = Book.find(params[:id])
+		@book.update(book_params)
+		redirect_to book_path(@book)
+	end
+
+	def destroy
+		@book = Book.find(prams[:id])
+		@book.destroy
+		rdirect_to books_path
+	end
+
+	private
+	def book_params
+		params.require(:book).permit(:title, :body,)
+	end
+end
+
